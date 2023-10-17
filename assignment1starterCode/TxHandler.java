19afea6dcdb169d306fcb0cdf8d3098aea409fe7
seanking <152>
2023-10-17 23:57:12 +0800

    first commit

---
+++ assignment1starterCode/TxHandler.java
@@ -0,0 +1,53 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class TxHandler {
+
+    private UTXOPool utxoPool;
+
+    /**
+     * Creates a public ledger whose current UTXOPool (collection of unspent transaction outputs) is
+     * {@code utxoPool}. This should make a copy of utxoPool by using the UTXOPool(UTXOPool uPool)
+     * constructor.
+     */
+    public TxHandler(UTXOPool utxoPool) {
+        this.utxoPool = new UTXOPool(utxoPool);
+    }
+
+    /**
+     * @return true if:
+     * (1) all outputs claimed by {@code tx} are in the current UTXO pool, 
+     * (2) the signatures on each input of {@code tx} are valid, 
+     * (3) no UTXO is claimed multiple times by {@code tx},
+     * (4) all of {@code tx}s output values are non-negative, and
+     * (5) the sum of {@code tx}s input values is greater than or equal to the sum of its output
+     *     values; and false otherwise.
+     */
+    public boolean isValidTx(Transaction tx) {
+        if (tx == null) return false;
+
+        Set<UTXO> doubleSpentSet = new HashSet<>(); 
+        for (Transaction.Input in : tx.getInputs()) {
+            // check if input originates from UTXOPool and if double spent and if input siganture is valid
+            UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
+            if (!utxoPool.contains(utxo) || doubleSpentSet.contains(utxo)) return false;
+            doubleSpentSet.add(utxo);
+
+            // check if input siganture is valid
+            
+        }
+
+        return true;
+    }
+
+    /**
+     * Handles each epoch by receiving an unordered array of proposed transactions, checking each
+     * transaction for correctness, returning a mutually valid array of accepted transactions, and
+     * updating the current UTXO pool as appropriate.
+     */
+    public Transaction[] handleTxs(Transaction[] possibleTxs) {
+        // IMPLEMENT THIS
+        return null;
+    }
+
+}
